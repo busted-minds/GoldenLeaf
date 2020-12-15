f7f3f62fa3254834c002587c856ce6a5ccf24980
Gampsi <Khroma@protonmail.com>
2020-12-15 15:33:00 -0400

    Golden Leaf 0.1 Part 1

--- src/System/Js/someJS.js
+++ src/System/Js/someJS.js
@@ -25,8 +25,16 @@ Config.saves.onLoad = function(save) {
 	if(Config.saves.version !== window.lastSaveVersion){
 		window.saveUpdate = true;
 		Config.saves.version = window.lastSaveVersion;
-	}
+    }
+    location.reload();
 };
+
+var l = document.createElement('link');
+l.rel = "icon";
+l.href = "img/sidebar-icons/arousal.png";
+l.type = "img/png";
+
+document.head.appendChild(l);
 
 window.saveUpdate = true;
 
@@ -38,3 +46,65 @@ window.saveUpdate = true;
 //    return false;
 //}
 //document.getElementById("CarList").onchange = setCar;
+
+(function () {
+    // disable.js, by chapel, for sugarcube 2
+    // v1.0.0
+
+    'use strict';
+
+    var cls = 'disabled';
+    var interactive = ['button', 'fieldset', 'input', 'menuitem', 'optgroup', 'option', 'select', 'textarea'];
+
+    function getEl (self) {
+        // get the first interactive element
+        var $el = $(self).find(interactive.join(',')).first();
+        if (!$el[0]) {
+            $el = $(self).children().eq(0);
+            if (!$el[0]) {
+                return $(self);
+            }
+        }
+        return $el;
+    }
+
+    function changeCls ($el) {
+        if ($el.ariaIsDisabled()) {
+            $el.addClass(cls);
+        } else {
+            $el.removeClass(cls);
+        }
+    }
+
+    function disable ($el, bool) {
+        if (!($el instanceof $)) {
+            $el = $($el);
+        }
+        $el.ariaDisabled((bool === undefined) ? true : !!bool);
+        changeCls($el);
+        return $el;
+    }
+
+    // no need for JS API as there us a built-in jQuery extension
+
+    Macro.add('disable', {
+        tags : null,
+        handler : function () {
+            var bool, $wrapper = $(document.createElement('span'))
+                .addClass('macro-' + this.name)
+                .wiki(this.payload[0].contents);
+
+            try {
+                bool = this.args.raw.trim() ? !!Scripting.evalJavaScript(this.args.full) : undefined;
+            } catch (err) {
+                return this.error("bad evaluation: " + err.message);
+            }
+
+            disable(getEl($wrapper), bool);
+
+            // output
+            $(this.output).append($wrapper);
+        }
+    });
+
+}());
